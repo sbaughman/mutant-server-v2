f07fabd9f7ba14aa7d748d4804cea92571034948
Seth Baughman <sbaughman1@me.com>
2016-06-18 19:42:26 -0400

    Add method to listen for new texts being created on Firebase

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ ref.authWithCustomToken(process.env.FIREBASE_SECRET, function(error, authData) {
   }
 });
 
+// Listen for new texts being created on Firebase
+var textsRef = ref.child('texts');
+textsRef.on('child_added', function(snapshot) {
+  console.log(snapshot.val());
+})
+
 server.listen(3030, function() {
   console.log('listening on http://localhost:3030...');
 });
