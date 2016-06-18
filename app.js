0db5b98054d468569db9c57ff7ab60f82ced7823
Seth Baughman <sbaughman1@me.com>
2016-06-18 19:18:35 -0400

    add comments.  require Firebase

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+
+
+// Create references for libraries
 var express = require('express');
 var http = require('http');
+var Firebase = require('firebase');
 
+// Express server setup
 var app = express();
 var server = http.createServer(app);
+
+// Authenticate Firebase
+
+
 
 server.listen(3030, function() {
   console.log('listening on http://localhost:3030...');
