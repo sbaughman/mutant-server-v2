4ddd60a5510c16b2dbf99722668f120ba7b12dda
Seth Baughman <sbaughman1@me.com>
2016-06-18 19:24:31 -0400

    adds Firebase authentication

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 // Create references for libraries
 var express = require('express');
 var http = require('http');
@@ -10,8 +8,14 @@ var app = express();
 var server = http.createServer(app);
 
 // Authenticate Firebase
-
-
+var ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com/");
+ref.authWithCustomToken(AUTH_TOKEN, function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Login Succeeded!", authData);
+  }
+});
 
 server.listen(3030, function() {
   console.log('listening on http://localhost:3030...');
